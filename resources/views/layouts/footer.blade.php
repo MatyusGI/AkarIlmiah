5b121b638119c2d18f83626661714be035e7f1e4
Matyus <jonijagu@gmail.com>
2021-02-13 19:34:17 +0700

    buat komponen livewire

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <hr>
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col text-center">
+                Copyright @2020 E-shop.com
+            </div>
+        </div>
+    </div>
+    
+</footer>
